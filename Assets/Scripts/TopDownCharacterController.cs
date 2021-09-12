d82c3ebec4364828a2d33c1b7142e6d9be6b0e07
NicolasH-L <nhonglao@gmail.com>
2021-09-11 23:12:31 -0400

    AI des ennemis en progression

--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -1,14 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
     private const string KeyMoveRight = "d";
     private const string KeyMoveLeft = "a";
     private const float PlayerSpeed = 3f;
+    private const float DelayTime = 0.6f;
+    private const string AttackInputKey = "space";
+
+    [SerializeField] private GameObject judahWeapon;
     private Animator _animatorPlayer;
+    private List<Animator> _liste;
     private bool _isMovingRight;
+    private bool _hasAttacked;
 
     private static readonly int IsIdle = Animator.StringToHash("isIdle");
+    private static readonly int AttackTrigger = Animator.StringToHash("Attack");
+
 
     public delegate void GameFinished(bool isDead);
 
@@ -21,7 +32,9 @@ public class TopDownCharacterController : MonoBehaviour
         //     OnGameEnded += GameManager.GameManagerInstance.GameOver;
         // }
         _isMovingRight = true;
-        _animatorPlayer = GetComponent<Animator>();
+        _liste = new List<Animator>();
+        _liste.AddRange(GetComponentsInChildren<Animator>());
+        _animatorPlayer = _liste[1];
     }
 
     private void Update()
@@ -39,16 +52,32 @@ public class TopDownCharacterController : MonoBehaviour
         dir.Normalize();
         GetComponent<Rigidbody2D>().velocity = PlayerSpeed * dir;
 
-        // if (Input.GetKeyUp(KeyMoveRight) || Input.GetKeyUp(KeyMoveLeft) || !Input.GetKey(KeyMoveRight) &&
-        //     !Input.GetKey(KeyMoveLeft))
-        //     SetIdle(true);
+        if (Input.GetKey(AttackInputKey) && !_hasAttacked)
+            Attack();
+        else if (Input.GetKeyUp(AttackInputKey) ||
+                 !Input.GetKey(AttackInputKey))
+            judahWeapon.SetActive(false);
+    }
+
+    private void Attack()
+    {
+        if (_hasAttacked)
+            return;
+        _animatorPlayer.SetTrigger(AttackTrigger);
+
+        judahWeapon.SetActive(true);
+        _hasAttacked = true;
+        StartCoroutine(ResetDelayNextAttack());
+    }
+
+    private IEnumerator ResetDelayNextAttack()
+    {
+        yield return new WaitForSeconds(DelayTime);
+        _hasAttacked = false;
     }
 
     private void FixedUpdate()
     {
-        var movementPlayerX = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerSpeed;
-        // if (movementPlayerX == 0) return;
-        // SetIdle(false);
         if (Input.GetKey(KeyMoveRight) && !_isMovingRight && !Input.GetKey(KeyMoveLeft))
             ChangeDirection();
         else if (Input.GetKey(KeyMoveLeft) && _isMovingRight && !Input.GetKey(KeyMoveRight))

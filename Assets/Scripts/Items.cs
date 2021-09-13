f4cc79a66b963195ded3a7c3632676a605a82b19
NicolasH-L <nhonglao@gmail.com>
2021-09-13 15:38:15 -0400

    Level design optimized

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -5,8 +5,6 @@ public class Items : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {
             Destroy(gameObject);
-        }
     }
 }

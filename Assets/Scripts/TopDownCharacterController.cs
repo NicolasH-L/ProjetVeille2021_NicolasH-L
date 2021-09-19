94f966903a8cd36abbd6d6e6a2c919b509d84dec
NicolasH-L <nhonglao@gmail.com>
2021-09-18 23:58:21 -0400

    Script : Ajout de son et effets sonores pour le joueur

--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -14,16 +14,28 @@ public class TopDownCharacterController : MonoBehaviour
     private const string AttackInputKey = "space";
     private const float PlayerSpeed = 3f;
     private const float DelayTime = 0.6f;
+    private const int MaxHealth = 100;
+    private const int PlayerHitAudioSourceIndex = 0;
+    private const int AttackAudioSourceIndex = 1;
+    private const int SoundEffect2 = 1;
 
     [SerializeField] private GameObject judahWeapon;
+    [SerializeField] private HealthBar healthBar;
+    [SerializeField] private List<AudioClip> listAttackClips;
+
     private Animator _animatorPlayer;
     private List<Animator> _liste;
+    private AudioSource[] _audioSource;
+    private List<AudioSource> _audioSourceAttack;
+    private int _currentMaxHealth;
+    private int _currentHealth;
+    private int _audioClipIndex;
     private bool _isMovingRight;
     private bool _hasAttacked;
+    private bool _isHurtSoundPlayed;
 
     private static readonly int IsIdle = Animator.StringToHash("isIdle");
     private static readonly int AttackTrigger = Animator.StringToHash("Attack");
-
 
     public delegate void GameFinished(bool isDead);
 
@@ -36,7 +48,7 @@ public class TopDownCharacterController : MonoBehaviour
         else
             _player = this;
     }
-    
+
     private void Start()
     {
         if (GameManager.GameManagerInstance != null)
@@ -45,6 +57,15 @@ public class TopDownCharacterController : MonoBehaviour
         _liste = new List<Animator>();
         _liste.AddRange(GetComponentsInChildren<Animator>());
         _animatorPlayer = _liste[1];
+        _audioSource = GetComponents<AudioSource>();
+        _audioSourceAttack = new List<AudioSource>();
+        _audioSourceAttack.AddRange(GetComponents<AudioSource>());
+        _audioSourceAttack[AttackAudioSourceIndex].clip = listAttackClips[_audioClipIndex];
+        _audioClipIndex = 0;
+        _currentMaxHealth = MaxHealth;
+        _currentHealth = MaxHealth;
+        healthBar.SetMaxValue(_currentHealth);
+        healthBar.SetValue(_currentHealth);
     }
 
     private void Update()
@@ -69,12 +90,21 @@ public class TopDownCharacterController : MonoBehaviour
             judahWeapon.SetActive(false);
     }
 
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyMoveRight) && !_isMovingRight && !Input.GetKey(KeyMoveLeft))
+            ChangeDirection();
+        else if (Input.GetKey(KeyMoveLeft) && _isMovingRight && !Input.GetKey(KeyMoveRight))
+            ChangeDirection();
+    }
+
     private void Attack()
     {
         if (_hasAttacked)
             return;
         _animatorPlayer.SetTrigger(AttackTrigger);
-
+        _audioSourceAttack[SoundEffect2].Play();
+        _audioSourceAttack[AttackAudioSourceIndex].Play();
         judahWeapon.SetActive(true);
         _hasAttacked = true;
         StartCoroutine(ResetDelayNextAttack());
@@ -84,21 +114,45 @@ public class TopDownCharacterController : MonoBehaviour
     {
         yield return new WaitForSeconds(DelayTime);
         _hasAttacked = false;
+        ChangeAttackAudioClip();
     }
 
-    private void FixedUpdate()
+    private void ChangeAttackAudioClip()
     {
-        if (Input.GetKey(KeyMoveRight) && !_isMovingRight && !Input.GetKey(KeyMoveLeft))
-            ChangeDirection();
-        else if (Input.GetKey(KeyMoveLeft) && _isMovingRight && !Input.GetKey(KeyMoveRight))
-            ChangeDirection();
+        ++_audioClipIndex;
+        if (_audioClipIndex + 1 > listAttackClips.Count)
+            _audioClipIndex = 0;
+        _audioSource[AttackAudioSourceIndex].clip = listAttackClips[_audioClipIndex];
     }
-
-    private void SetIdle(bool isIdle)
+    
+    private void TakeDamage(int damage)
     {
-        _animatorPlayer.SetBool(IsIdle, isIdle);
+        if (!_isHurtSoundPlayed)
+        {
+            _audioSource[0].Play();
+            StartCoroutine(DelayHurtSound());
+        }
+
+        _currentHealth -= damage;
+        if (_currentHealth == 0)
+            OnGameEnded?.Invoke(true);
+        healthBar.SetValue(_currentHealth);
     }
 
+    private IEnumerator DelayHurtSound()
+    {
+        yield return new WaitForSeconds(DelayTime);
+        _isHurtSoundPlayed = false;
+    }
+
+    private void GainHP(int value)
+    {
+        _currentHealth = healthBar.GetCurrentValue() + value >= healthBar.GetCurrentMaxValue()
+            ? healthBar.GetCurrentMaxValue()
+            : healthBar.GetCurrentValue() + value;
+        healthBar.SetValue(_currentHealth);
+    }
+    
     private void ChangeDirection()
     {
         transform.Rotate(0, 180, 0);
@@ -111,6 +165,7 @@ public class TopDownCharacterController : MonoBehaviour
         {
             case Bullet:
                 transform.GetComponent<SpriteRenderer>().color = Color.red;
+                TakeDamage(10);
                 break;
             case TeleportationPoint:
                 var manager = GameManager.GameManagerInstance;

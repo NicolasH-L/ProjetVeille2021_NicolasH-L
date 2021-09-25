3797b3ef55040fbbb22c5e50ee8c17cdea83b070
NicolasH-L <nhonglao@gmail.com>
2021-09-25 19:11:22 -0400

    Script : Ajout du gestionnement de layer pour l'arme par rapport au sprite du joueur

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    [SerializeField] private TopDownCharacterController _player;
+    [SerializeField] private GameObject _weapon;
+
+    private void FixedUpdate()
+    {
+        switch (_player.gameObject.layer)
+        {
+            case 20:
+                _weapon.gameObject.layer = 20;
+                _weapon.GetComponent<SpriteRenderer>().sortingLayerName = "Layer 1";
+                break;
+            case 21:
+                _weapon.gameObject.layer = 21;
+                _weapon.GetComponent<SpriteRenderer>().sortingLayerName = "Layer 2";
+                break;
+            case 22:
+                _weapon.gameObject.layer = 22;
+                _weapon.GetComponent<SpriteRenderer>().sortingLayerName = "Layer 3";
+                break;
+        }
+
+        _weapon.GetComponent<SpriteRenderer>().sortingOrder = 3;
+    }
+}

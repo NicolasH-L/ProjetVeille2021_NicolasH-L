e13aa1280b66ac36f3330e05cf7726af0f0aa06e
NicolasH-L <nhonglao@gmail.com>
2021-09-26 16:38:05 -0400

    Script : Creation du script pour faire la fonctionnaliter du menu pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private const string DestroyGameManager = "Game Manager";
+    private const string DestroyPlayer = "PlayerTeri";
+    private const string DestroyPlayerUI = "PlayerUI";
+    private const string WelcomeScreenMenu = "WelcomeScreenMenu";
+    private const int FinalLevelScene = 3;
+    [SerializeField] private GameObject pauseMenuUI;
+    private bool _isGamePaused;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        _isGamePaused = false;
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        if (_isGamePaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Destroy(GameObject.Find(DestroyGameManager));
+        Destroy(pauseMenuUI);
+        Destroy(GameObject.Find(DestroyPlayer));
+        Destroy(GameObject.Find(DestroyPlayerUI));
+        SceneManager.LoadScene(WelcomeScreenMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (SceneManager.GetActiveScene().buildIndex <= FinalLevelScene) return;
+        Destroy(gameObject);
+    }
+}

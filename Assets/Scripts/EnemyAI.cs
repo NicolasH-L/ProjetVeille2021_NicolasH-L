6b44b6a864afb57ed4519c986cd930a31dcf639c
NicolasH-L <nhonglao@gmail.com>
2021-09-27 10:38:20 -0400

    Script : Optimisation du code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform target;
-    private const float SPEED = 200f;
-    private const float NEXT_WAYPOINT = 3F;
+    private const float Speed = 200f;
+    private const float NextWaypoint = 3F;
+    private const float SpriteScale = 0.2751575f;
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform spriteBoss;
     private Path _path;
-    private int _currentWaypoint = 0;
-    private bool _reachedEndPath = false;
     private Seeker _seeker;
     private Rigidbody2D _rigidbody2D;
+    private int _currentWaypoint;
+    private bool _reachedEndPath;
 
     void Start()
     {
         _seeker = GetComponent<Seeker>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
-
+        _reachedEndPath = false;
+        _currentWaypoint = 0;
         InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
@@ -37,8 +40,7 @@ public class EnemyAI : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_path == null)
-            return;
+        if (_path == null) return;
 
         if (_currentWaypoint <= _path.vectorPath.Count)
             _reachedEndPath = true;
@@ -46,21 +48,15 @@ public class EnemyAI : MonoBehaviour
             _reachedEndPath = false;
 
         Vector2 direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rigidbody2D.position).normalized;
-        Vector2 force = direction * SPEED * Time.deltaTime;
+        Vector2 force = direction * Speed * Time.deltaTime;
         _rigidbody2D.AddForce(force);
         float distance = Vector2.Distance(_rigidbody2D.position, _path.vectorPath[_currentWaypoint]);
-        if (distance < NEXT_WAYPOINT)
-        {
+        if (distance < NextWaypoint)
             _currentWaypoint++;
-        }
 
         if (_rigidbody2D.velocity.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
+            spriteBoss.localScale = new Vector3(-SpriteScale, SpriteScale, SpriteScale);
         else if (_rigidbody2D.velocity.x <= -0.01f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
+            spriteBoss.localScale = new Vector3(SpriteScale, SpriteScale, SpriteScale);
     }
 }
